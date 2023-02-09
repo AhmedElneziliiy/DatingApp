f412d8a09d83c03fbc5b76ced1d28811f1bd0ed7
ahmed <elneziliiyahmed@gmail.com>
2023-02-09 16:22:08 +0200

    Clean up Program Class

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using API.Data;
+using API.Extensions;
 using API.Interfaces;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -9,34 +10,21 @@ using Microsoft.IdentityModel.Tokens;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddScoped<ITokenService,TokenService>();
-
-builder.Services.AddDbContext<DataContext>(options=>{
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-
-
-
+builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
     {
         builder.WithOrigins("https://localhost:4200").AllowAnyMethod().AllowAnyHeader();
     }));
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options=>{
-        options.TokenValidationParameters=new TokenValidationParameters{
-            ValidateIssuerSigningKey=true,
-            IssuerSigningKey=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
-            ValidateIssuer=false,
-            ValidateAudience=false
-        };
-    });
+
+builder.Services.AddIdentityService(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
