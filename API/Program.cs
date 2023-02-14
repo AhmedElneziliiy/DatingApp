61c015c74fc60b5fb4ca8d61a45eedbf399d7409
ahmed <elneziliiyahmed@gmail.com>
2023-02-14 17:02:23 +0200

    Extending user entity and AutoMapper Configurations

--- API/Program.cs
+++ API/Program.cs
@@ -46,4 +46,22 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+
+//users seeding data
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try 
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+//
+
+
 app.Run();
