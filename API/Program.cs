6b34f03fcc9475a892726c8f46650cce625fdc7f
ahmed <elneziliiyahmed@gmail.com>
2023-03-15 20:54:36 +0200

    Added Identity and Admin Controller

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,12 @@
 using System.Text;
 using API.Data;
+using API.Entities;
 using API.Extensions;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
@@ -33,13 +35,6 @@ builder.Services.AddIdentityService(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-// if (app.Environment.IsDevelopment())
-// {
-//     app.UseSwagger();
-//     app.UseSwaggerUI();
-// }
 app.UseMiddleware<ExceptionMiddleware>();
 //
 app.UseHttpsRedirection();
@@ -58,8 +53,10 @@ var services = scope.ServiceProvider;
 try 
 {
     var context = services.GetRequiredService<DataContext>();
+    var userManager=services.GetRequiredService<UserManager<AppUser>>();
+    var roleManager=services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
-    await Seed.SeedUsers(context);
+    await Seed.SeedUsers(userManager,roleManager);
 }
 catch (Exception ex)
 {
